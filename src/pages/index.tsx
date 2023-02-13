cbb2a22f225050d62dc3b2cdcd66f6b3071c2c3b
marcelooo616 <marcelooo2014@hotmail.com>
2023-02-13 16:36:50 -0300

    :tada: Commit inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SujeitoPizza - Faca seu login</title>
+      </Head>
+      <div>
+        <h1>
+          Sujeito Pizza
+        </h1>
+      </div>
+    </>
+  )
+}
